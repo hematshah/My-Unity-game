15095388fe8c17cbff40e3614e778f10b9799897
Hemat Shah <hshah2@spartaglobal.com>
2019-11-25 00:01:33 +0000

    My Unity Game

---
+++ Assets/Script/NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;  // declaring and assigning a variable 
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+     int guess; 
+
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher() {
+
+        min = guess + 1; // defining "min" as "guess" it takes the variable data of 1 and replaces it with 500  
+        NextGuess();
+
+    }
+
+    public void OnPressLower() {
+
+        max = guess;
+        NextGuess();//fuction used to call the repeated statements
+
+    }
+
+ 
+
+    void NextGuess()
+    {
+
+        guess = Random.Range(min , max+1);
+        guessText.text = guess.ToString();
+
+    }
+}
---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene() {
+
+        // declared and defined an integer variable called "CurrentSceneIndex". it is within #
+        // SceneManager class, we're saying run the method 
+        //  ActiveScene, we need to know whats our current 
+        //buildIndex of the scene we are currently on
+
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+    }
+
+    public void LoadStartScene() {
+
+       
+        SceneManager.LoadScene(0); 
+    }
+
+    public void QuitScene() {
+
+        Application.Quit();
+
+    }
+}
